9b4a18f05d0b946b2d51e1399dfcd2cc343a2dc6
Imzan Khan <ikhan5@lakeheadu.ca>
2019-05-19 23:24:30 -0400

    Added PHP Mailer functionality

--- Script/scripts.js
+++ Script/scripts.js
@@ -1,3 +1,28 @@
 $(document).ready(function () {
     $('.sidenav').sidenav();
+    $("#contact__form").submit(function (event) {
+        event.preventDefault();
+        let name = $("#contact__name").val();
+        let email = $("#contact__email").val();
+        let message = $("#contact__message").val();
+        $.ajax({
+            url: "functions/sendEmail.php",
+            method: "POST",
+            data: {
+                email: email,
+                name: name,
+                message: message
+            },
+            beforeSend: function (data) {
+                $("#email_conf").html("<p class='green-text'>Sending...</p>");
+            },
+            success: function (response) {
+                $("#email_conf").html("<p class='green-text'>Email sent successfully, thank you for your time !");
+            },
+            error: function (response) {
+                $("#email_conf").html("<p class='red-text'>problem sending email, please try again.");
+            }
+        });
+    });
+
 });
---
+++ functions/sendEmail.php
@@ -0,0 +1,54 @@
+<?php
+/* Author: Imzan Khan
+ * Feature: Notifications
+ * Description: Sets up the email settings for contact form
+ * Date Created: May 19th, 2019
+ * Last Modified: May 19th, 2019
+ * Recent Changes: Access code changed to be more simple
+ */
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+date_default_timezone_set('America/Toronto');
+require '../vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+
+function format($input)
+{
+    $formatted = trim($input);
+    $formatted = stripslashes($input);
+    $formatted = htmlspecialchars($input);
+    return $formatted;
+}
+$username = format($name);
+$email = format($email);
+$content = format($message);
+$output = '';
+$subject = "Message from " . $username;
+$mail = new PHPMailer;
+$mail->Host = 'a2plcpnl0588.prod.iad2.secureserver.net';
+$mail->isSMTP();
+$mail->Port = '587';
+$mail->SMTPAuth = true;
+$mail->Username = "landing@imzankhan.ca";
+$mail->Password = "";
+$mail->SMTPSecure = 'tls';
+$mail->setFrom('landing@imzankhan.ca', $username);
+$mail->addAddress("ikhan5@lakeheadu.ca", "Imzan Khan");
+$mail->WordWrap = 50;
+$mail->isHTML(true);
+$mail->Subject = $subject;
+$mail->Body = "<h2>Email from $email</h2>
+<h2>Name: $username</h2>
+<p style='font-size:16px;'><br />$content</p>";
+$mail->AltBody = '';
+$result = $mail->Send();
+
+if ($result["code"] == '400') {
+    $output .= html_entity_decode($result['full_error']);
+}
+if ($output == '') {
+    echo 'Success';
+} else {
+    echo $output;
+}
