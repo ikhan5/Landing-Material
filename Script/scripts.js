e74d60c01abeaf83a4074ea9f95191b206735caa
Imzan Khan <imzan@mindtrades.com>
2019-11-19 16:48:07 -0500

    Remove PHP files

--- Script/scripts.js
+++ Script/scripts.js
@@ -1,29 +1,45 @@
 $(document).ready(function () {
     $('.sidenav').sidenav();
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
     $("#contact__form").submit(function (event) {
         event.preventDefault();
-        let name = $("#contact__name").val();
-        let email = $("#contact__email").val();
-        let message = $("#contact__message").val();
+        let email = $("#email").val();
+        let password = $("#password").val();
+
+        if(email && password){
+            $("#api_conf").html("<p class='green-text'>Please fill in all fields ...</p>");
+        }
         $.ajax({
-            url: "functions/sendEmail.php",
             method: "POST",
-            data: {
-                email: email,
-                name: name,
-                message: message
-            },
+            url: proxyurl +"https://blooming-meadow-78473.herokuapp.com/api/auth/",
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify({ email: email, password: password }),
             beforeSend: function (data) {
-                $("#email_conf").html("<p class='green-text'>Sending...</p>");
+                $("#api_conf").html("<p class='green-text'>Retrieving Data (may take a few seconds)...</p>");
+                $(".resume-details").hide();
             },
             success: function (response) {
-                $("#email_conf").html("<p class='green-text'>Email sent successfully, thank you for your time !");
+                $("#api_conf").html("<p class='green-text'>Successfully Retrieved Data</p>");
+                $.ajax({
+                    method: "GET",
+                    url: proxyurl+"https://blooming-meadow-78473.herokuapp.com/api/resume/",
+                    headers: { "x-auth-token": response.token },
+                    dataType: "json",
+                    contentType: "application/json; charset=utf-8",
+                    success: function(response) {
+                      var str = JSON.stringify(response, null, 2);
+                      $(".resume-details").show();
+                      $(".resume-details").html(str);
+                    }
+                  });
             },
             error: function (response) {
-                $("#email_conf").html("<p class='red-text'>problem sending email, please try again.");
+                $(".resume-details").hide();
+                $("#api_conf").html("<p class='red-text'>Problem accessing data from the API, check credentials and try again.");
             }
         });
     });
-
 });
+
